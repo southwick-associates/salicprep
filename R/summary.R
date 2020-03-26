89ab9237c02848b2da5a0c5734493f28730f722a
Daniel Kary <dankary@gmail.com>
2020-03-26 12:41:32 -0400

    summary_lic_types

--- R/summary.R
+++ R/summary.R
@@ -28,28 +28,38 @@ plot_demo <- function(df, vars) {
     )
 } 
 
-#' Make a heatmap of license type customers by year
+#' Count license type customers by year
 #' 
 #' @param df data frame with sale data and license type info
 #' @param types types to include (based on "type" variable)
+#' @family functions to summarize license data
+#' @export
+#' @examples 
+#' # sale <- left_join(sale, lic, by = "lic_id")
+#' # summary_lic_types(sale)
+summary_lic_types <- function(df, types = c("hunt", "fish", "combo")) {
+    df %>%
+        filter(.data$type %in% c("hunt", "fish", "combo")) %>%
+        distinct(.data$cust_id, .data$year, .data$description) %>%
+        count(.data$year, .data$description)
+}
+
+#' Make a heatmap of license type customers by year
+#' 
+#' @param df Count of license types as returned by \code{\link{summary_lic_types}}
 #' @param truncate Truncation point for "description" variable. If NULL, no
 #' truncation will be performed.
 #' @family functions to summarize license data
 #' @export
 #' @examples 
 #' # sale <- left_join(sale, lic, by = "lic_id")
-#' # plot_lic_types(sale)
-plot_lic_types <- function(
-    df, types = c("hunt", "fish", "combo"), truncate = 30
-) {
-    x <- df %>%
-        filter(.data$type %in% c("hunt", "fish", "combo")) %>%
-        distinct(.data$cust_id, .data$year, .data$description) %>%
-        count(.data$year, .data$description)
+#' # df <- summary_lic_types(sale)
+#' # plot_lic_types(df)
+plot_lic_types <- function(df, truncate = 30) {
     if (!is.null(truncate)) {
-        x$description <- substr(x$description, 1, truncate)
+        df$description <- substr(df$description, 1, truncate)
     }
-    ggplot(x, aes_string("year", "description", fill = "n")) + 
+    ggplot(df, aes_string("year", "description", fill = "n")) + 
         geom_tile() +
         theme(
             legend.position = "none",
